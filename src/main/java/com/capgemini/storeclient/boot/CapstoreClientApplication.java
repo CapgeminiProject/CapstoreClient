9a624d283df6fe1cac3640b02b33a4ae909257df
Lalit Nikhare <lalitn246@gmail.com>
2018-08-17 14:30:12 +0530

    Initial Project

---
+++ src/main/java/com/capgemini/storeclient/boot/CapstoreClientApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.storeclient.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoreClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoreClientApplication.class, args);
+	}
+}
